c69dbdbc914a834b6baf891015bebd537af97135
John Baracuda <58704151+JohnBaracuda@users.noreply.github.com>
2024-01-27 14:11:48 +0100

    Inertia Update

---
+++ Runtime/DOTS/NativeExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+
+namespace MobX.Utilities.DOTS
+{
+    public static class NativeExtensions
+    {
+        public static ref T GetRef<T>(this NativeArray<T> array, int index)
+            where T : struct
+        {
+            if (index < 0 || index >= array.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+            unsafe
+            {
+                return ref UnsafeUtility.ArrayElementAsRef<T>(array.GetUnsafePtr(), index);
+            }
+        }
+
+        public static ref T GetRef<T>(this NativeList<T> list, int index)
+            where T : unmanaged
+        {
+            if (index < 0 || index >= list.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+            unsafe
+            {
+                return ref UnsafeUtility.ArrayElementAsRef<T>(list.GetUnsafePtr(), index);
+            }
+        }
+    }
+}
